87205248dcc71b4b3044e701d6e3c497eeccf4e4
Daniel-Keller-HPC-AI <Daniel.Keller.1@stonybrook.edu>
2024-11-26 21:06:32 -0600

    Create solution.py

---
+++ 01_intro_AI_on_Supercomputer/solution.py
@@ -0,0 +1,57 @@
+# set our initial slope and intercept
+m = 5.
+b = 1000.
+# batch_size = 60
+# set a learning rate for each parameter
+learning_rate_m = 1e-7
+learning_rate_b = 1e-1
+# use these to plot our progress over time
+loss_history = []
+# convert panda data to numpy arrays, one for the "Ground Living Area" and one for "Sale Price"
+data_x = data['GrLivArea'].to_numpy()
+data_y = data['SalePrice'].to_numpy()
+# we run our loop N times
+loop_N = 30
+for i in range(loop_N):
+   # update our slope and intercept based on the current values
+   m = updated_m(data_x,data_y,m,b,learning_rate_m)
+   b = updated_b(data_x,data_y,m,b,learning_rate_b)
+
+   # calculate the loss value
+   loss_value = np.mean(loss(data_x,data_y,m,b))
+
+   # keep a history of our loss values
+   loss_history.append(loss_value)
+
+   # print our progress
+   print('[%03d]  dy_i = %.2f * x + %.2f     previously calculated: y_i = %.2f * x + %.2f    loss: %f' % (i,m,b,m_calc,b_calc,loss_value))
+   
+   # close/delete previous plots
+   plt.close('all')
+   
+   # create a 1 by 2 plot grid
+   fig,ax = plt.subplots(1,2,figsize=(18,6),dpi=80)
+   # lot our usual output
+   plot_data(data_x,data_y,m,b,ax[0])
+
+   # here we also plot the calculated linear fit for comparison
+   line_x = np.arange(data_x.min(),data_x.max())
+   line_y = line_x * m_calc + b_calc
+   ax[0].plot(line_x,line_y,'b-',label='calculated')
+   # add a legend to the plot and x/y labels
+   ax[0].legend()
+   ax[0].set_xlabel('square footage')
+   ax[0].set_ylabel('sale price')
+
+   # plot the loss 
+   loss_x = np.arange(0,len(loss_history))
+   loss_y = np.asarray(loss_history)
+   ax[1].plot(loss_x,loss_y, 'o-')
+   ax[1].set_yscale('log')
+   ax[1].set_xlabel('loop step')
+   ax[1].set_ylabel('loss')
+   plt.show()
+   # gives us time to see the plot
+   time.sleep(2.5)
+   # clears the plot when the next plot is ready to show.
+   ipydis.clear_output(wait=True)
